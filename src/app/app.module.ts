29881dde9c80f35b4f151f8f3f9b214f23cd35aa
sdclayton <96846005+sdclayton@users.noreply.github.com>
2022-10-17 20:25:35 -0500

    Started todoApp, worked on building a todo form in form-todo.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosComponent } from './todos/todos.component';
+import { FormTodoComponent } from './todos/form-todo/form-todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    FormTodoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/todos/form-todo/form-todo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-todo',
+  templateUrl: './form-todo.component.html',
+  styleUrls: ['./form-todo.component.css']
+})
+export class FormTodoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
